105e7c3ccd7ec82dac14455ab90c8221f59a877f
Segolene Alquier <segolene.alquier@yahoo.com>
2020-04-16 16:59:05 +0200

    changed design of lobby page

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -1,18 +1,22 @@
 <template>
-  <form class="lobby">
-    <div>
-      <label>Player 1</label>
-      <input type="text" placeholder="Name for player 1" v-model="player1Name" />
-    </div>
-    <div>
-      <label>Player 2</label>
-      <input type="text" placeholder="Name for player 2" v-model="player2Name" />
-    </div>
+  <div class="lobby-wrapper">
+    <h1>Tic Tac Toe</h1>
+    <img class="lobby-logo" src="tic-tac-toe.png" width="100px" />
+    <form class="lobby">
+      <div>
+        <label>Player 1</label>
+        <input type="text" placeholder="Name for player 1" v-model="player1Name" />
+      </div>
+      <div>
+        <label>Player 2</label>
+        <input type="text" placeholder="Name for player 2" v-model="player2Name" />
+      </div>
 
-    <div class="lobby__action">
-      <button @click="start" type="submit">Start the game</button>
-    </div>
-  </form>
+      <div class="lobby__action">
+        <button @click="start" type="submit">Start the game</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
