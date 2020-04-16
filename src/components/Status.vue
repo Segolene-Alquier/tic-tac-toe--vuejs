6ebf7f99f86ddbdfeff0da0e78c4032e5800d4fe
Segolene Alquier <segolene.alquier@yahoo.com>
2020-04-16 16:24:49 +0200

    small fix : when no player name is entered, then it takes either "X" or "O" so that there is no empty string displayed

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -15,11 +15,15 @@ export default {
   },
   computed: {
     playerName() {
-      const { player1Name, player2Name } = this.$store.state;
+      let { player1Name, player2Name } = this.$store.state;
+      if (player1Name === "") player1Name = "X";
+      if (player2Name === "") player2Name = "O";
       return this.player === "X" ? player1Name : player2Name;
     },
     winnerName() {
-      const { player1Name, player2Name } = this.$store.state;
+      let { player1Name, player2Name } = this.$store.state;
+      if (player1Name === "") player1Name = "X";
+      if (player2Name === "") player2Name = "O";
       return this.winner === "X" ? player1Name : player2Name;
     }
   }
